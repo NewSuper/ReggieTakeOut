71a087a296b7d6645422e1e13981abc2f37679ec
NewSuper <newsuper6892@sina.com>
2022-08-22 18:17:44 +0800

    地址薄

---
+++ src/main/java/cn/tqxd/reggie/entity/AddressBookDao.java
@@ -0,0 +1,9 @@
+package cn.tqxd.reggie.entity;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import org.apache.ibatis.annotations.Mapper;
+
+//地址
+@Mapper
+public interface AddressBookDao extends BaseMapper<AddressBook> {
+}
---
+++ src/main/java/cn/tqxd/reggie/service/impl/AddressBookServiceImpl.java
@@ -0,0 +1,11 @@
+package cn.tqxd.reggie.service.impl;
+
+import cn.tqxd.reggie.entity.AddressBook;
+import cn.tqxd.reggie.entity.AddressBookDao;
+import cn.tqxd.reggie.service.AddressBookService;
+import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import org.springframework.stereotype.Service;
+
+@Service
+public class AddressBookServiceImpl extends ServiceImpl<AddressBookDao, AddressBook> implements  AddressBookService {
+}
