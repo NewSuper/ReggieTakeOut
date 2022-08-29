579205d9f4aa2f0af8e84ba51a5c8684524c013d
NewSuper <newsuper6892@sina.com>
2022-08-29 18:42:49 +0800

    用户

---
+++ src/main/java/cn/tqxd/reggie/service/UserService.java
@@ -0,0 +1,7 @@
+package cn.tqxd.reggie.service;
+
+import cn.tqxd.reggie.entity.User;
+import com.baomidou.mybatisplus.extension.service.IService;
+
+public interface UserService extends IService<User> {
+}
